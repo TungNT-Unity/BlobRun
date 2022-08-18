b6bf63b25e4f1a6da9e61ece7de7120abfc64468
TungNguyen <tungnt@wildthunder.org>
2022-08-18 14:28:23 +0700

    - projectile weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform projectilePrefabs;
-    
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private int waveNumber = 1;
+    [SerializeField] private float delayWave = 0f;
+    [SerializeField] private int projectileNumber = 1;
+    [SerializeField] private float minAngel = 0f;
+    [SerializeField] private float maxAngle = 0f;
+
     public void Fire()
     {
         
+    }
+
+    IEnumerator StartWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            float angle = maxAngle - minAngel;
+            float angleStep = angle / projectileNumber;
+            for (int j = 0; j < projectileNumber; j++)
+            {
+                Transform cloneProjectile = Instantiate(projectilePrefabs);
+                cloneProjectile.position = firePoint.position;
+                //cloneProjectile.rotation = Quaternion.LookRotation()
+            }
+            yield return new WaitForSeconds(delayWave);
+        }
     }
 }
